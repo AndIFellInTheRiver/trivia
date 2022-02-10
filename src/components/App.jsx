3e0915cd75141546be2ee376daa3162624a7812b
AndIFellInTheRiver <61436979+AndIFellInTheRiver@users.noreply.github.com>
2022-02-09 17:53:32 -0800

    Day 1 finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,30 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  console.log(data[0].question.choices.length);
+  var questionNum = 0;
+  return (
+    <div className="app">
+      <h1>Trivia!</h1>
+      <Questions number={questionNum} />
+      <NextQuestion />
+    </div>
+  );
 }
 
+function Questions(props) {
+  return (
+    <div className="question">
+      {data[props.number].question.text}
+      <div className="answers">{data[props.number].question.choices[0]}</div>
+    </div>
+  );
+}
+function NextQuestion(props) {
+  return (
+    <div className="nextButton">
+      <button className="next"> Next Question </button>
+    </div>
+  );
+}
 export default App;
