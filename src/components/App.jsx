153bf6400d878f94cf4acb7d2893e835b58a5cd4
AndIFellInTheRiver <61436979+AndIFellInTheRiver@users.noreply.github.com>
2022-03-02 17:57:15 -0800

    day 3 progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,71 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+// let [ questionNum, setquestionNum ] = useState(0);
+
 function App() {
   console.log(data[0].question.choices.length);
-  var questionNum = 0;
+  const [questionNum, setquestionNum] = useState(0);
   return (
     <div className="app">
       <h1>Trivia!</h1>
       <Questions number={questionNum} />
-      <NextQuestion />
+      <NextQuestion number={questionNum} />
+      <CorrectAnswer number={questionNum} />
     </div>
   );
 }
 
 function Questions(props) {
+  /*
+      {data[props.number].question.choices[0]}
+      */
   return (
     <div className="question">
       {data[props.number].question.text}
-      <div className="answers">{data[props.number].question.choices[0]}</div>
+      {data[props.number].question.choices.map((choice) => (
+        <Answer choiceText={choice} />
+      ))}
     </div>
   );
 }
+
+//  <button onClick={() => setPopulation(population + 1)}>+</button>,
+
 function NextQuestion(props) {
   return (
     <div className="nextButton">
-      <button className="next"> Next Question </button>
+      <button className="next" onClick={() => setquestionNum(props.number + 1)}>
+        {" "}
+        Next Question{" "}
+      </button>
+    </div>
+  );
+}
+
+function Answer(props) {
+  return (
+    <div className="answers">
+      <span>{props.choiceText}</span>
+    </div>
+  );
+}
+
+function CorrectAnswer(props) {
+  const [answerShow, setanswerShow] = useState(false);
+  return (
+    <div className="CorrectAnswer">
+      <button onClick={() => setanswerShow(true)}>Show Answer</button>
+      {answerShow && (
+        <p>
+          The correctanswer is{" "}
+          {
+            data[props.number].question.choices[
+              data[props.number].question.correct_choice_index
+            ]
+          }{" "}
+        </p>
+      )}
     </div>
   );
 }
